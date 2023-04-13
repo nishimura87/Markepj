032f6414b4239acbaa314fd77d503ed09d7b63d6
nishimura87 <nishimura.k87@gmail.com>
2023-04-13 23:26:27 +0900

    s3関連部修正

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -78,7 +78,10 @@ class NewsController extends Controller
         $form['user_id'] =  Auth::id();
 
             if(isset($request->img_path)){
-                $image = $request->img_path;
+                //$image = $request->img_path;
+
+                //s3利用の場合
+                $image = Storage::disk('s3')->url($request->img_path);
             }
             else{
                 $image = null;
